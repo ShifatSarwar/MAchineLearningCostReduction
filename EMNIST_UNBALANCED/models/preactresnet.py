d793622d60545780f2f8a33e5c8da1c2e94e62cd
ShifatSarwar <57101801+ShifatSarwar@users.noreply.github.com>
2022-10-25 16:40:15 -0500

    Add files via upload

---
+++ EMNIST_UNBALANCED/models/preactresnet.py
@@ -0,0 +1,319 @@
+import os
+import math
+import torch
+import torchvision
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchvision.transforms as tt
+from torch.utils.data import random_split
+
+
+# Assign GPU for process
+gpuCore = 3
+# Assign Number of epochs
+epochs = 100
+# Mode 1 for entire Dataset
+# Mode 2 for half the Dataset
+# Mode 3 for Similar Dataset
+mode = 3
+
+# Choose Dataset
+dataset1 = 'cifar10'
+dataset2 = 'emnist'
+dataset3 = ''
+dataset4 = ''
+dataset5 = ''
+
+data_dir = './data/data'+str(mode)+'/' + dataset1
+
+# Data transforms (normalization & data augmentation)
+stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+train_tfms = tt.Compose([tt.RandomCrop(32, padding=4, padding_mode='reflect'), 
+                         tt.RandomHorizontalFlip(), 
+                         tt.ToTensor(), 
+                         tt.Normalize(*stats,inplace=True)])
+valid_tfms = tt.Compose([tt.ToTensor(), tt.Normalize(*stats)])
+
+# PyTorch datasets
+train_ds = ImageFolder(data_dir+'/train', train_tfms)
+valid_ds = ImageFolder(data_dir+'/test', valid_tfms)
+
+batch_size = 400
+
+# PyTorch data loaders
+train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=3, pin_memory=True)
+valid_dl = DataLoader(valid_ds, batch_size*2, num_workers=3, pin_memory=True)
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 12))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images[:64], nrow=8).permute(1, 2, 0))
+        break
+
+
+def get_params(params, device):
+    new_params = [p.to(device) for p in params]
+    for p in new_params:
+        p.requires_grad_()
+    return new_params
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda:'+str(gpuCore))
+    else:
+        return torch.device('cpu')
+    
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+device = get_default_device()
+train_dl = DeviceDataLoader(train_dl, device)
+valid_dl = DeviceDataLoader(valid_dl, device)
+
+# Confusion_matrix generator
+y_pred = []
+y_true = []
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+
+def accuracy2(outputs, labels):
+    _, preds = torch.max(outputs, dim=1) 
+    output = preds
+    label = labels
+    output = output.data.cpu().numpy()
+    y_pred.extend(output)
+    label = label.data.cpu().numpy()
+    y_true.extend(label) # Save Truth
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+    
+    def validation_step2(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy2(out, labels)           # Calculate accuracy
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_acc']))
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class PreActBlock(nn.Module):
+    '''Pre-activation version of the BasicBlock.'''
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += shortcut
+        return out
+
+
+class PreActBottleneck(nn.Module):
+    '''Pre-activation version of the original Bottleneck module.'''
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(PreActBottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.conv3(F.relu(self.bn3(out)))
+        out += shortcut
+        return out
+
+
+class PreActResNet(ImageClassificationBase):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(PreActResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def PreActResNet18():
+    return PreActResNet(PreActBlock, [2,2,2,2])
+# Choose Model
+model = to_device(PreActResNet18(), device)
+
+# Main evaluator
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+# Use for Confusion Matrix
+def evaluate2(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step2(batch) for batch in val_loader]
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+def fit_one_cycle(epochs, max_lr, model, train_loader, val_loader, 
+                  opt_func=torch.optim.SGD):
+    torch.cuda.empty_cache()
+    history = []
+    
+    # Set up cutom optimizer with weight decay
+    optimizer = opt_func(model.parameters(), max_lr)
+    # Set up one-cycle learning rate scheduler
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs, 
+                                                steps_per_epoch=len(train_loader))
+    
+    for epoch in range(epochs):
+        # Training Phase 
+        model.train()
+        train_losses = []
+        lrs = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            
+            # Gradient clipping
+            # if grad_clip: 
+                # nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+            
+            optimizer.step()
+            optimizer.zero_grad()
+            
+            # Record & update learning rate
+            lrs.append(get_lr(optimizer))
+            sched.step()
+        
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+
+max_lr = 0.01
+# grad_clip = 0.1
+# weight_decay = 1e-4
+opt_func = torch.optim.Adam
+
+def getHistory(val):
+    history = [evaluate(model, valid_dl)]
+    # history += fit_one_cycle(epochs, max_lr, model, train_dl, valid_dl, 
+    #                          grad_clip=grad_clip, 
+    #                          weight_decay=weight_decay, 
+    #                          opt_func=opt_func)
+    history += fit_one_cycle(epochs, max_lr, model, train_dl, valid_dl,
+                            opt_func=opt_func)
+    evaluate2(model, valid_dl)
+    torch.save(model.state_dict(), 'saved_Models/model_'+val+'.sav')
+    return history,y_true, y_pred
+
+
+
